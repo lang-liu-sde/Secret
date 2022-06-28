ec006f8f8192d4657ceffb01256168aeb874bf25
lang-liu-sde <lliu65d92@gmail.com>
2022-06-28 11:10:20 -0700

    level 5 - cookie & session

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
@@ -18,8 +21,17 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+  // cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB");
+// mongoose.set("useCreateIndex", true);
 
 ///////////////////////////////mongoose encryption/////////////////////////////////////
 // an object created from mongoose schema class
@@ -35,7 +47,13 @@ const userSchema = new mongoose.Schema ({
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // home page
 app.get("/", function(req, res) {
@@ -53,52 +71,133 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-// only if user signed register page, input his email & password, will render secrets page
-app.post("/register", function(req, res) {
-  // using bcrypt
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+// different from the tutorial
+app.get("/logout", function(req, res) {
+  req.logout(function(err) {
+    if (!err) {
+      res.redirect("/");
+    } else {
+      console.log(err);
+    }
 
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    // when call save, a. store to mongodb; b. encrypt password
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
   });
-  //
 
 });
 
-// validate if the database has such a user
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  // decrypt password when call find
-  User.findOne({email: username}, function(err, foundUser) {
+
+//////////////////////////////////////below is for level 4 - hashing & salting/////////////////////////
+// only if user signed register page, input his email & password, will render secrets page
+// app.post("/register", function(req, res) {
+//   // using bcrypt
+//   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//
+//     const newUser = new User({
+//       email: req.body.username,
+//       password: hash
+//     });
+//     // when call save, a. store to mongodb; b. encrypt password
+//     newUser.save(function(err) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render("secrets");
+//       }
+//     });
+//   });
+//   //
+// });
+//
+// // validate if the database has such a user
+// app.post("/login", function(req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   // decrypt password when call find
+//   User.findOne({email: username}, function(err, foundUser) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       if (foundUser) {
+//         // using bcrypt
+//         bcrypt.compare(password, foundUser.password, function(err, result) {
+//           if (result === true) {
+//             res.render("secrets");
+//           } else {
+//             console.log(err);
+//           }
+//         });
+//           // console.log(foundUser.password);
+//       }
+//     }
+//   });
+// });
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////below is level 5 - cookie & session/////////////////////////
+app.post("/register", function(req, res) {
+  // register() method comes from the passport-local-mongoose package
+  // js object with {} in it.
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
     if (err) {
       console.log(err);
+      res.direct("/register");
     } else {
-      if (foundUser) {
-        // using bcrypt
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("secrets");
-          } else {
-            console.log(err);
-          }
-        });
-          // console.log(foundUser.password);
-      }
+      // authenticate our user using passport
+      passport.authenticate("local")(req, res, function() {
+        // Here is the different point from the previous procedure
+        // previous: res.render("/secrets") after register & login
+        // current: we can direct to route "/secrets" after authenticating, therefore we need app.get("/secrets")
+        res.redirect("/secrets");
+      });
     }
   });
 });
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  // this method comes from passport
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      // send cookie and tell the browser to hold on that cookie
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
